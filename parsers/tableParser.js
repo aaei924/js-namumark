8d1556e352e2b4e31644fa3e92a4b19f6c57195a
PRASEOD- <aaei924@gmail.com>
2021-03-23 03:12:43 +0900

    Update tableParser.js
    주석 추가

--- parsers/tableParser.js
+++ parsers/tableParser.js
@@ -69,6 +69,7 @@ module.exports = (wikitext, pos, setpos) => {
     // 시발 표 존나 복잡하네
     // 버그 : || 2행이 {{{ || }}} 되어야 하는데 || 3행으로 됨 ㅋ ||
     let caption = null;
+    // ||로 시작하지 않으면 캡션부터 처리
     if (!wikitext.substring(pos).startsWith('||')) {
         caption = wikitext.substring(pos + 1, wikitext.indexOf('|', pos + 2));
         pos = wikitext.indexOf('|', pos + 1) + 1;
@@ -76,11 +77,13 @@ module.exports = (wikitext, pos, setpos) => {
     } else {
         pos += 2;
     }
+    // || 기준으로 끊어서 컬럼 분할
     let cols = wikitext.substring(pos).split('||'),
         rowno = 0,
         hasTableContent = false,
         colspan = 0,
         rowspan = 0;
+    // <|2> 같은 표 옵션 정규식
     let optionPattern = /<(.+?)>/;
     console.log(cols);
     let table = {
@@ -89,18 +92,24 @@ module.exports = (wikitext, pos, setpos) => {
     let tableOptions = {};
     // parse cols, result= {wikitext, options, rowOptions} => table
     let i;
+    // js의 length = PHP의 count()
+    // 열이 하나일때
     if(cols.length < 2)
         return null;
     for (i = 0; i < cols.length; i++) {
+        // PHP의 foreach($col as $cols)와 비슷
         let col = cols[i],
             curColOptions = {},
             rowOption = {};
+        // 개행하는 지점에서 끊기
         if (col.startsWith('\n') && col.length > 1) {
             // table end
             break;
         }
+        // 개행하는 지점에서 다음 행으로 넘어가기
         if (col == '\n') {
             // new row
+            // 행번호 1 추가
             table[++rowno] = [];
             continue;
         }
@@ -109,12 +118,15 @@ module.exports = (wikitext, pos, setpos) => {
             colspan++;
             continue;
         }
+        // || 좌측정렬||우측정렬 || 가운데정렬 ||
         if (col.startsWith(' ') && !col.endsWith(' '))
             curColOptions["text-align"] = "left"
         else if (!col.startsWith(' ') && col.endsWith(' '))
             curColOptions["text-align"] = "right"
         else if (col.startsWith(' ') && col.endsWith(' '))
             curColOptions["text-align"] = "middle"
+
+        // preg_match() 함수와 유사 (옵션 찾기)
         while (optionPattern.test(col)) {
             // 옵션이 존재함.
             let match = optionPattern.exec(col);
@@ -125,23 +137,30 @@ module.exports = (wikitext, pos, setpos) => {
             curColOptions = extend(true, curColOptions, colOptions_set);
             tableOptions = extend(true, tableOptions, tableOptions_set);
             rowOptions_set = extend(true, rowOption, rowOptions_set);
+            
+            // colspan = colspan + colspan_add
             colspan += colspan_add;
             rowspan += rowspan_add;
 
             if (tableOptions["border-color"]) {
+                // 인자에 맞게 설정 후 인자 삭제
                 tableOptions["border"] = `2px solid ${tableOptions["border-color"]}`;
                 delete tableOptions["border-color"];
             }
             //if (matched) {
+                // 컬럼 변수에 옵션부분 제외하고 할당
                 col = col.substring(match[0].length);
             //}
         }
         let colObj = {options: curColOptions, colspan: colspan, rowspan: rowspan, rowOption: rowOption, wikitext: col};
         colspan = 0; rowspan = 0;
         table[rowno].push(colObj);
+        // row 배열속에 col 들어있는 구조 생성
         hasTableContent = true;
+        // 내용있음 표시
     }
     // gen row options
+    // table 배열에는 row 개수만큼 배열이 들어 있음. 이 배열 안에는 row 설정이 들어가 있음.
     let rowOptions = [];
     for (let j = 0; j < table.length; j++) {
         let rowOption = {};
@@ -152,6 +171,7 @@ module.exports = (wikitext, pos, setpos) => {
     }
     // return as tokens
     let result = [{name:"table-start", options: tableOptions}];
+    // table 배열 속 키 개수 = row 개수
     let rowCount = Object.keys(table).length;
     for (let j = 0; j < rowCount; j++) {
         result.push({name: "table-row-start", options: rowOptions[j]});
@@ -164,8 +184,17 @@ module.exports = (wikitext, pos, setpos) => {
     }
     result.push({name:"table-end"});
     if(hasTableContent) {
+        // 컬럼 사이에 || 끼워둠
         setpos(pos + cols.slice(0, i).join('||').length + 1)
         return result;
+        // result배열 구조:
+// table-start, 테이블옵션
+// ㄴ table-row-start, row옵션
+//    ㄴ table-col-start, col옵션, colspan, rowspan - wikitext, 본문내용, line취급여부 - table-col-end
+//    ... col 개수만큼 반복
+// ㄴ table-row-end
+// ... row 개수만큼 반복
+// table-end
     } else {
         return null;
     }

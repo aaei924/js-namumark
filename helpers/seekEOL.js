27e7f8166f26cec862bd7e27c06b672cefb1ccf4
PRASEOD- <aaei924@gmail.com>
2021-03-23 03:38:45 +0900

    Update seekEOL.js

--- helpers/seekEOL.js
+++ helpers/seekEOL.js
@@ -1,3 +1,4 @@
 module.exports = function seekEOL(text, offset = 0) {
+    // 맨 마지막 문자가 \n이라면 텍스트 길이는 \n이 있는 위치임.
     return text.indexOf('\n', offset) == -1 ? text.length : text.indexOf('\n', offset);
 }
